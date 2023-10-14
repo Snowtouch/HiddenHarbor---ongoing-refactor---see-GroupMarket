81cafc01a77e47cfa3f6195aa125638633367c94
Snowtouch <138367829+Snowtouch@users.noreply.github.com>
2023-10-15 01:36:58 +0200

    Create KoinAppModule.kt

---
+++ app/src/main/java/com/snowtouch/hiddenharbor/di/KoinAppModule.kt
@@ -0,0 +1,64 @@
+package com.snowtouch.hiddenharbor.di
+
+import androidx.compose.material3.SnackbarHostState
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.snowtouch.hiddenharbor.R
+import com.snowtouch.hiddenharbor.data.repository.RealtimeDatabaseServiceImpl
+import com.snowtouch.hiddenharbor.ui.components.SnackbarGlobalDelegate
+import com.snowtouch.hiddenharbor.viewmodel.AccountScreenViewModel
+import com.snowtouch.hiddenharbor.viewmodel.FavoritesScreenViewModel
+import com.snowtouch.hiddenharbor.viewmodel.UserState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+//VARIABLE TO KEEP PERSONAL DATA HIDDEN
+val firebaseDatabaseLocalAddress = R.string.firebaseDatabaseLocal.toString()
+val firebaseDatabaseRemoteAddress = R.string.firebaseDatabaseRemote.toString()
+val firebaseStorageLocalAddress = R.string.firebaseStorageLocal.toString()
+
+val firebaseModule = module {
+
+    single {
+        if (isFirebaseLocal) {
+            FirebaseDatabase.getInstance(firebaseDatabaseLocalAddress)
+        } else {
+            FirebaseDatabase.getInstance(firebaseDatabaseRemoteAddress)
+        }
+    }
+
+    single {
+        val auth = FirebaseAuth.getInstance()
+        if (isFirebaseLocal) {
+            auth.useEmulator("10.0.2.2", 9099)
+        }
+        auth
+    }
+
+    single {
+        if (isFirebaseLocal) {
+            FirebaseStorage.getInstance(firebaseStorageLocalAddress)
+        } else {
+            FirebaseStorage.getInstance()
+        }
+    }
+
+    single { RealtimeDatabaseServiceImpl(get()) }
+
+    single { UserState }
+}
+val viewModelModule = module {
+    viewModel { AccountScreenViewModel(get(), get(), get()) }
+    viewModel { FavoritesScreenViewModel(get()) }
+}
+
+val snackbarHostModule = module {
+    single { SnackbarHostState() }
+    single { CoroutineScope(SupervisorJob() + Dispatchers.Main) }
+    single { SnackbarGlobalDelegate(get(), get()) }
+}
+
+var isFirebaseLocal = true
